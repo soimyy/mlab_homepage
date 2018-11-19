79dd82340fe328ae365911f39bba6a5d0063bb42
soimyy <dayanbiefu7@gmail.com>
2018-11-19 21:05:08 +0900

    version3

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8sda1qj@whimfynh9s*-4-a^%!ng031(ug=j)e(3#%lr%+_81-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost","192.168.100.100"]
+ALLOWED_HOSTS = ["133.54.247.115","localhost","127.0.0.1"]
 
 
 # Application definition
--- homepage/news/models.py
+++ homepage/news/models.py
@@ -16,10 +16,8 @@ class Post(models.Model):
 
     title = models.CharField('タイトル',max_length=255)
     text = models.TextField('本文')
-    image = models.ImageField('写真',upload_to='media/',blank=True,)
+    image = models.ImageField('写真',upload_to='media/',blank=True,default="media/media/no_image.jpg")
     category = models.ForeignKey(Category,verbose_name='カテゴリ',on_delete=models.PROTECT)
-    # date = str(timezone.now()).replace("-",".").split(" ")[0]
-    # created_at = models.CharField('作成日',max_length=20,default=date)
     created_at = models.DateTimeField('作成日',max_length=20,default=timezone.now)
 
     def __str__(self):
