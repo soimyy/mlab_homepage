3353f6a3b09ccae1d6f65ce667c2195685721640
soimyy <dayanbiefu7@gmail.com>
2018-11-20 10:46:23 +0900

    version4

--- homepage/news/admin.py
+++ homepage/news/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Category,Post
+from .models import Category,Post,Blog,Image
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Category)
+# admin.site.register(Blog)
+# admin.site.register(Image)
--- homepage/news/models.py
+++ homepage/news/models.py
@@ -16,9 +16,38 @@ class Post(models.Model):
 
     title = models.CharField('タイトル',max_length=255)
     text = models.TextField('本文')
-    image = models.ImageField('写真',upload_to='media/',blank=True,default="media/media/no_image.jpg")
+    image = models.ImageField('写真',upload_to='media/',blank=True,default="media/no_image.jpg")
     category = models.ForeignKey(Category,verbose_name='カテゴリ',on_delete=models.PROTECT)
     created_at = models.DateTimeField('作成日',max_length=20,default=timezone.now)
 
     def __str__(self):
         return self.title
+
+
+class Blog(models.Model):
+    """記事"""
+    title = models.CharField('タイトル', max_length=255)
+    text = models.TextField('本文')
+    category = models.ForeignKey(
+        Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+    thumnail = models.ImageField(
+        'サムネイル', upload_to='post_thumbnail/%Y/%m/%d/', blank=True, null=True)
+    is_publick = models.BooleanField('公開可能か?', default=True)
+    friend_posts = models.ManyToManyField(
+        'self', verbose_name='関連記事', blank=True)
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    """記事に紐づく画像ファイル"""
+    title = models.CharField('タイトル', max_length=255, blank=True)
+    post = models.ForeignKey(
+    Blog, verbose_name='記事', on_delete=models.PROTECT,
+    )
+    src = models.ImageField('画像', upload_to='images/%Y/%m/%d/', help_text='送信後、一度保存してください。')
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+        return '間接リンク:[filter imgpk]{0}[end] 直接リンク:[filter img]{1}[end]'.format(self.pk, self.src.url)
